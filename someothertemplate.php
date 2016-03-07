3745919c28c6304bf056f4d2c1ba4d8a513f48c6
denlillemand <mattinielsen5@hotmail.com>
2016-03-07 13:40:20 +0100

    herp

---
+++ someothertemplate.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once './vendor/autoload.php';
+
+$loader = new Twig_Loader_Filesystem('./templates');
+
+$twig = new Twig_Environment($loader);
+
+echo $twig->render('somechildtemplate.twig', array(
+    'name' => 'Fabien',
+    'header' => 'My webpage!',
+    'foo' => array(
+	'bar' => 'bar',
+    ),
+    'data' => array(
+	'data-foo' => 'herpderp',
+    ),
+));
+
